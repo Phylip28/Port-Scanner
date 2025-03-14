957711bed3a6205313d96d79bc5490a919e5092b
Phylip28 <felipe28769@gmail.com>
2025-03-13 23:25:47 -0500

    start updates

--- port_scanner.py
+++ port_scanner.py
@@ -1,14 +1,15 @@
 import os
 import sys
 
-def scan_ports(target):
-	print(f"Escaneando puertos en {target}...")
-	os.system(f"nmap -p- -sV {target}")
 
-if __name__ == "__main__":
-	if len(sys.argv) != 2:
-		print("Forma de uso -> python3 port_scanner.py <direccion IP o dominio>")
-		sys.exit(1)
+def scan_ports(ip):
+    print(f"Escaneando puertos en {target}...")
+    os.system(f"nmap -p- -sV {target}")
 
-	target = sys.argv[1]
-	scan_ports(target)
+
+if len(sys.argv) != 2:
+    print("Forma de uso -> python3 port_scanner.py <direccion IP o dominio>")
+    sys.exit(1)
+
+target = sys.argv[1]
+scan_ports(target)
